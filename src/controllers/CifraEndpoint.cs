e3f48a938905de94df259ab45864e4557b135198
MaluAbrami <marialuizaabrami@gmail.com>
2025-11-06 20:18:40 -0300

    ajuste endpoints

--- src/controllers/CifraEndpoint.cs
+++ src/controllers/CifraEndpoint.cs
@@ -1,19 +1,26 @@
+using src.models;
+using src.services;
+
 namespace src.controllers
 {
     public static class CifraEndpoint
     {
         public static void MapCifraEndpoints(this IEndpointRouteBuilder app)
         {
-            var group = app.MapGroup("/");
+            var group = app.MapGroup("").WithTags("Cifra de César");
 
-            group.MapPost("/cifrar", () =>
+            group.MapPost("/cifrar", (CifraService service, CifrarRequest request) =>
             {
-                return Results.Ok();
+                var result = service.Cifrar(request);
+
+                return Results.Ok(result);
             });
 
-            group.MapPost("/decifrar", (int id) =>
+            group.MapPost("/decifrar", (CifraService service, DecifrarRequest request) =>
             {
-                return Results.Ok();
+                var result = service.Decifrar(request);
+
+                return Results.Ok(result);
             });
 
             group.MapPost("/decifrarForcaBruta", () =>
