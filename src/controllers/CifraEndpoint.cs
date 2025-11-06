4cf4fcf09cd31fbad4400e9fda7353deb576e035
MaluAbrami <marialuizaabrami@gmail.com>
2025-11-06 19:29:24 -0300

    add endpoints

---
+++ src/controllers/CifraEndpoint.cs
@@ -0,0 +1,25 @@
+namespace src.controllers
+{
+    public static class CifraEndpoint
+    {
+        public static void MapCifraEndpoints(this IEndpointRouteBuilder app)
+        {
+            var group = app.MapGroup("/");
+
+            group.MapPost("/cifrar", () =>
+            {
+                return Results.Ok();
+            });
+
+            group.MapPost("/decifrar", (int id) =>
+            {
+                return Results.Ok();
+            });
+
+            group.MapPost("/decifrarForcaBruta", () =>
+            {
+                return Results.Created();
+            });
+        }
+    }
+}
