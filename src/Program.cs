52df54195a709106dc7addcf54f74a768ec8a7c1
MaluAbrami <marialuizaabrami@gmail.com>
2025-11-13 19:16:19 -0300

    chore: add cors

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddLogging();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddOpenApi();
 builder.Services.AddScoped<CifraService>();
 builder.Services.AddScoped<DicionarioAbertoValidator>();
@@ -15,6 +25,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseCors("AllowAll");
 
 app.MapCifraEndpoints();
 
